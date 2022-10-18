5511046b7e450bcfc68eb40467c952b1d9629cc5
tjoeun <tjoeun@host.docker.internal>
2022-10-18 17:33:38 +0900

    User v1

---
+++ ShoppingMallUser/src/main/java/com/tj/shopping/user/index.java
@@ -0,0 +1,21 @@
+package com.tj.shopping.user;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class index {
+	
+	@RequestMapping(value= {"/","/index","/index.do","/index.html"})
+	public String home(Model m, HttpServletRequest req) {
+		String idx = "123";
+		
+		m.addAttribute("i",idx);
+		System.out.println(idx);
+		
+		return "user/index/index";
+	}
+}
---
+++ ShoppingMallUser/src/main/java/com/tj/shopping/user/item/item_sql.java
@@ -0,0 +1,24 @@
+package com.tj.shopping.user.item;
+
+import java.util.*;
+
+import javax.annotation.Resource;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service("it")
+public class item_sql {
+	item_dao ido = null;
+
+	@Resource(name="sqlTemp")
+	private SqlSessionTemplate session;
+	
+	public item_dao list_select (int num){
+		ido = new item_dao();
+		ido.setPidx(num);
+		item_dao list = session.selectOne("sql.item_sel",ido);
+		return list;
+	}
+}
